9818ee147b08288e7fdd5c142ef7d0d301db48a6
PAmarnathreddy <110371788+PAmarnathreddy@users.noreply.github.com>
2022-08-01 10:53:24 +0530

    Base framework

---
+++ Amazon test/src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+import generics.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void testLogin(){
+
+    }
+}
